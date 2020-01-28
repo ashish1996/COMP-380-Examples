9a0472af4efb600894962a93def78c16b7572297
Ashish Khanchandani <akhanchandani1996@gmail.com>
2020-01-27 22:38:12 -0500

    initial commit

---
+++ src/Arrays.java
@@ -0,0 +1,21 @@
+
+public class Arrays {
+	
+	public static void main(String[] args) {
+		printArray();
+	}
+	
+	public static void printArray() {
+		
+		int[] testArray= {5,4,3,2,1};
+		
+		for(int i = 0; i<testArray.length; i++) {
+	
+		System.out.println(testArray[i]);
+		
+		
+		
+	}
+	
+}
+}
---
+++ src/loopsAndConditionals.java
@@ -0,0 +1,17 @@
+
+public class loopsAndConditionals {
+
+	public static void main(String[] args) {
+		printUsingIf();
+	}
+	
+	public static void printUsingIf() {
+		
+		int test=10;
+		
+	for(int i = 0; i<test; i++) {
+		System.out.println("Hello World!");
+	}
+	
+}
+}
